9facdc94f34963a959e7d6cb0393084be0fdcd65
Kaltrinë Baliu <93614313+kaltrinabaliu@users.noreply.github.com>
2024-11-11 17:34:13 +0100

    Update qrcode.js

--- qrcode.js
+++ qrcode.js
@@ -1,1 +1,10 @@
+const forge = require('node-forge');
+const QRCode = require('qrcode');
+
+function generateKeyPair() {
+    const ed25519 = forge.pki.ed25519;
+    const keyPair = ed25519.generateKeyPair();
+    return keyPair;
+}
+
 
