ccb65d11bfa4b0905bc690a5131c315c40c76d17
njomzaarexhepii@gmail.com <75095687+NjomzaRexhepi@users.noreply.github.com>
2024-11-12 09:30:09 +0100

    adding the signature

--- qrcode.js
+++ qrcode.js
@@ -1,13 +1,21 @@
 const forge = require('node-forge');
 const QRCode = require('qrcode');
 
+//function to generateKetPair
 function generateKeyPair() {
     const ed25519 = forge.pki.ed25519;
     const keyPair = ed25519.generateKeyPair();
     return keyPair;
 }
 
+//function to sign the message
 function signMessage(message, privateKey){
-    
+    const ed25519 = forge.pki.ed25519;
+    const messageBytes = Buffer.from(message, 'utf8');
+    const signature = ed25519.sign({
+        message: messageBytes,
+        privateKey: privateKey
+    });
+    return Buffer.from(signature).toString('hex');
 }
 
