a974daa6aebef44cc13447bebe48cc04dde19e69
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2024-11-13 15:27:29 +0100

    project created and install dependencies

---
+++ srcc/example/QRCode.java
@@ -0,0 +1,31 @@
+package example;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+import java.security.Security;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Signature;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+public class QRCode {
+
+	public static void main(String[] args) throws Exception {
+		 // Shto Bouncy Castle si provider
+        Security.addProvider(new BouncyCastleProvider());
+
+        // Krijo çelësin privat dhe publik
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EdDSA");
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        // Mesazhi për të cilin do të nënshkruajmë
+        String message = "Pershendetje!";
+        
+      }}
