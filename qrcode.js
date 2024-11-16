efc57368fcd8b196163dde5ca29765dcab52a3a9
njomzaarexhepii@gmail.com <75095687+NjomzaRexhepi@users.noreply.github.com>
2024-11-16 18:29:44 +0100

    deleted unnecessary code

--- qrcode.js
+++
@@ -1,9 +0,0 @@
-const forge = require('node-forge');
-const QRCode = require('qrcode');
-
-function generateKeyPair() {
-    const ed25519 = forge.pki.ed25519;
-    const keyPair = ed25519.generateKeyPair();
-    return keyPair;
-}
-
